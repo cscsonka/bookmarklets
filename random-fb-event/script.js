1ab98b04e7f1aa99ba661199203d60ceeb5c0f26
cscsonka <76667017+cscsonka@users.noreply.github.com>
2023-02-23 17:24:37 +0100

    Update script.js

--- random-fb-event/script.js
+++ random-fb-event/script.js
@@ -1,5 +1,7 @@
 // Opening a random Facebook event
 //location.href = "https://www.facebook.com/events/3467539816905698";
-console.log("FB event opened");
+function print_it(){
+    console.log("FB event opened");
+}
 
-
+print_it();
